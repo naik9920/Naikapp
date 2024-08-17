6b9c1a944f3f371389b907b50afbb98fd6fa5bd3
naik9920 <jagannaikiit@gmail.com>
2024-08-17 14:40:19 +0530

    naikapp created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CourcesComponent } from './cources/cources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    CourcesComponent
   ],
   imports: [
     BrowserModule,
